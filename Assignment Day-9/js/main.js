b60f2259b7d164ae1b09b3f7c9fd467a216c65ed
Gabe Rubio <gtrubio192@gmail.com>
2015-01-16 15:17:12 -0600

    completed rot13Unencrypted() function for lower and upper case scenarios

--- Assignment Day-9/js/main.js
+++ Assignment Day-9/js/main.js
@@ -16,20 +16,146 @@ function randElement(arr){
 }
 
 // #2
-function rot13Unencrypted()
+// Encrypt incoming string
+function rot13Unencrypted(text)
 {
+	var asciiValue = 0; 
+	// var rot13 = 26;
+	var upperAscii = '';
+	var lowerAscii = '';
 
+	var unencryptedName = text;
+
+	var encryptedName = '';
+
+	// asciiValue = unencryptedName.charCodeAt(0);		// returns ascii # @ position
+	// console.log("Before loop: " + asciiValue); 
+
+	for(var i=0; i<unencryptedName.length; i++) {
+		asciiValue = unencryptedName.charCodeAt(i);		// returns ascii # @ position
+		// check to see if upper case
+		if(asciiValue >= 65 && asciiValue <= 90)
+		{
+			console.log("Upper before sending off: " + asciiValue + " " + text[i]); 
+
+			upperAscii = upperEncrypt(asciiValue);
+			encryptedName = encryptedName + String.fromCharCode(upperAscii);
+		}
+		// or lower case
+		else if(asciiValue >=97 && asciiValue <=122)
+		{
+			console.log("Lower before sending off: " + asciiValue + " " + text[i]); 
+
+			lowerAscii = lowerEncrypt(asciiValue);
+			encryptedName = encryptedName + String.fromCharCode(lowerAscii);
+		}
+		else
+		{
+			throw "Invalid special character: " + unencryptedName[i];
+		}
+	}
+	return encryptedName;
+}
+
+function upperEncrypt(asciiValue)
+{
+	// reset starts at A == 65
+	var reset = 65;
+	var upperHigh = 90;
+
+	console.log("Upper AsciiValue Before +13 :" + asciiValue);
+	asciiValue += 13;								// adds 13 to ascii
+	console.log("Upper AsciiValue after +13: " + asciiValue)
+
+	if(asciiValue > upperHigh)
+	{
+		reset += asciiValue - upperHigh;
+		// asciiValue = unencryptedName.charCodeAt(i);
+		asciiValue = reset;							// give circular ascii value
+		return asciiValue;
+		// encryptedName = encryptedName + String.fromCharCode(asciiValue);
+	}
+	else{
+
+		// asciiValue = unencryptedName.charCodeAt(i);
+		// asciiValue += 13;
+		// encryptedName = encryptedName + String.fromCharCode(asciiValue);
+		return asciiValue;
+	}
+
+	// console.log(encryptedName);
+	// return encryptedName;
+
+}
+	
+function lowerEncrypt(asciiValue)
+{
+	// reset starts at a == 97
+	var reset = 97;
+	var lowerHigh = 122;
+
+	console.log("Lower AsciiValue Before +13 :" + asciiValue);
+	asciiValue += 13;								// adds 13 to ascii
+	console.log("Lower AsciiValue after +13: " + asciiValue)
+
+	if(asciiValue > lowerHigh)
+	{
+		reset += asciiValue - lowerHigh;
+		asciiValue = reset;
+		console.log("Returning Lower AsciiValue: " + asciiValue)
+
+		return asciiValue;
+	}
+	else
+	{
+		console.log("Returning Lower AsciiValue: " + asciiValue)
+		return asciiValue;
+	}
 }
 
 // #3
-function rot13Encrypted()
+function rot13Encrypted(text)
 {
+	var asciiValue = 0; 
+	var reset = 0;
+	var rot13 = 26;
+
+	var unencryptedName = text;
+
+	var encryptedName = '';
+
+	for(var i=0; i<unencryptedName.length; i++) {
+		// check to see if ascii val > 26
+		asciiValue = unencryptedName.charCodeAt(i);		// returns ascii # @ position
+		asciiValue -= 13;								// adds 13 to ascii
+		if(asciiValue < 0)
+		{
+			reset = rot13 + asciiValue;
+			// asciiValue = unencryptedName.charCodeAt(i);
+			asciiValue = reset;							// give circular ascii value
+			encryptedName = encryptedName + String.fromCharCode(asciiValue);
+		}
+		else{
+
+			// asciiValue = unencryptedName.charCodeAt(i);
+			// asciiValue -= 13;
+			encryptedName = encryptedName + String.fromCharCode(asciiValue);
+		}
+
+	}
+	console.log(encryptedName);
 
 }
 
 // #4
-function rotN_Unencrypted (argument) {
-	// body...
+function rotN_Unencrypted (text, n) {
+	var asciiValue = 0; 
+	var reset = 0;
+	var rot13 = n;
+
+	var unencryptedName = text;
+
+	var encryptedName = '';
 }
 
 // #5
@@ -37,7 +163,13 @@ function rotN_Unencrypted (argument) {
 // and returns an array of randomly selected pairs of students (array of arrays).
 function studentPairs(studArray)
 {
+	var len = studArray.length;
+	var stud1 = studArray[randomGen(len, 0)];
+	var stud2 = studArray[randomGen(len, 0)];
+	console.log("student array: " + stud1);
+	var pairs = [stud1, stud2];
 
+	return pairs; 
 }
 
 // #6
@@ -45,7 +177,9 @@ function studentPairs(studArray)
 // character using .split and .join.
 function stringDashes(text)
 {
-
+	var newText = text.split("").join("-");
+	console.log("Dashes: " + newText);
+	return newText;
 }
 
 // #7
